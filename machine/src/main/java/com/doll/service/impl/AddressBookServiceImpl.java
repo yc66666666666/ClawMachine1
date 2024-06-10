f62c0f5dbb9a76f98be3d6ec7b6ed26303cdfc09
yaocheng <3492810070@qq.com>
2024-06-10 20:50:20 +0800

    地址3

--- machine/src/main/java/com/doll/service/impl/AddressBookServiceImpl.java
+++ machine/src/main/java/com/doll/service/impl/AddressBookServiceImpl.java
@@ -6,18 +6,23 @@ import com.doll.common.BaseContext;
 import com.doll.entity.AddressBook;
 import com.doll.mapper.AddressBookMapper;
 import com.doll.service.AddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AddressBookServiceImpl extends ServiceImpl<AddressBookMapper, AddressBook> implements AddressBookService {
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+
     @Override
     @Transactional
     public AddressBook setDefault(AddressBook addressBook) {
+
         Long id=addressBook.getId();
         LambdaQueryWrapper<AddressBook> queryWrapper=new LambdaQueryWrapper<>();
-        queryWrapper.eq(AddressBook::getUserId, BaseContext.getCurrentId());
-        System.out.println("用户的iddddddddddd"+BaseContext.getCurrentId());
+        queryWrapper.eq(AddressBook::getUserId, addressBookMapper.selectById(id).getUserId());
+        System.out.println("用户的iddddddddddd"+addressBookMapper.selectById(id).getUserId());
         addressBook.setIsDefault(0);
         addressBook.setId(null);
         super.update(addressBook,queryWrapper);
