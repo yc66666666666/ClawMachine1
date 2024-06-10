a8a5c5dee2c14d57c6c299ba9d2190210cc05110
yaocheng <3492810070@qq.com>
2024-06-10 13:40:53 +0800

    验证码失效时间改为60分钟

--- machine/src/main/java/com/doll/controller/UserController.java
+++ machine/src/main/java/com/doll/controller/UserController.java
@@ -38,7 +38,7 @@ public class UserController {
             String code= ValidateCodeUtils.generateValidateCode(6).toString();
             SMSUtils.sendMessage("泉州寰游科技","SMS_300165018",phone,code);
 //            session.setAttribute(phone,code);   //把验证码存放在session里
-            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);  //把验证码存放在redis里，有效期为5分钟
+            redisTemplate.opsForValue().set(phone,code,60, TimeUnit.MINUTES);  //把验证码存放在redis里，有效期为5分钟
             return R.success("手机验证码发送成功");
         }
         return R.error("短信发送失败");
